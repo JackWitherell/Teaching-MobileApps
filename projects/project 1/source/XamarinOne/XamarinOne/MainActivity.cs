632788ea048d3ab60d098fa4ac09ed6b7d64ee5b
Unknown <JackWitherell@gmail.com>
2018-01-22 16:50:20 -0800

    Project One Initial Commit
    Oh baby I hear the blues are callin, toss NameSpaces and scrambled TextSpaces

---
+++ projects/project 1/source/XamarinOne/XamarinOne/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace XamarinOne
+{
+    [Activity(Label = "PostFix Calculator", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            var label = FindViewById<TextView>(id:Resource.Id.textView1);
+
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+            Button[] buttons = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += delegate {
+                label.Text = $"You clicked {count} times.";
+                button.Text = string.Format("{0} clicks!", count++);
+            };
+        }
+    }
+}
+
